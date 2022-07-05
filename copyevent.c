306573983ef37a8a3056e2894cd0223eb81c2de1
Auric Vente <mprops@protonmail.com>
2022-07-04 22:58:51 -0600

    Info mods

--- copyevent.c
+++ copyevent.c
@@ -1,4 +1,5 @@
 // Based on a merge request for clipnotify that wasn't merged after years
+// Merge request was made by andreblanke for the cdown repo
 // It allows using the -s flag like: copyevent -s clipboard,primary,secondary
 // Compilation: gcc copyevent.c -o copyevent -I/usr/X11R6/include -L/usr/X11R6/lib -lX11 -lXfixes
 
